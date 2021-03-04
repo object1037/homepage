e4d0e84fc817ec7b39c05a304c2fea359c21392a
object_1037 <yamagami.10370409@gmail.com>
2021-03-04 19:22:54 +0900

    polish style

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-      <Image src="/../public/images/profile.jpg" alt="my profile picture" width="400" height="400" /> 
-      <h1>object1037</h1>
-    </>
+    <div className="flex flex-col">
+      <Head>
+        <title>object_1037</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="justify-center object-none flex-shrink flex items-center mb-2 mt-4">
+        <div className="flex justify-center content-center rounded-full border-solid border-2 border-gray-300">
+          <Image priority src="/images/profile.jpg" alt="my profile picture" width="200" height="200" className="rounded-full" /> 
+        </div>
+      </div>
+      <h1 className="font-sans font-bold text-center text-4xl m-2">object_1037</h1>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
+    borderRadius: {
+      'full': '9999px',
+    },
     extend: {},
   },
   variants: {
