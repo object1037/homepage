5aed7b02504665fe516079c643ca4b9f6bce3d17
object_1037 <yamagami.10370409@gmail.com>
2021-03-05 23:30:37 +0900

    modify Image width & height

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     </Head>
     <section className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-10 bg-gray-100 dark:bg-gray-800 py-8">
       <div className="flex rounded-full shadow-xl">
-        <Image priority src="/images/profile.jpg" alt="my profile picture" width="200" height="200" className="rounded-full" /> 
+        <Image priority src="/images/profile.jpg" alt="my profile picture" width={200} height={200} className="rounded-full" /> 
       </div>
       <h1 className="font-sans font-bold text-4xl m-2 text-gray-900 dark:text-gray-100 min-h-full">object_1037</h1>
     </section>
