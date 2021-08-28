55648e08180eeb9b62755a3fc1de75fdc24476cb
object1037 <contact@object1037.dev>
2021-08-28 12:41:22 +0900

    update fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja" className="text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-        <Head />
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Inter-var.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
