07ec63a45b3cc5cc25146b4b7dd6350ca71289d8
object1037 <contact@object1037.dev>
2021-10-11 17:11:07 +0900

    update footer design

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,19 @@
 import IconLink from './icon-link'
-import { FaTwitter, FaGithub } from 'react-icons/fa'
+import { FiTwitter, FiGithub } from 'react-icons/fi'
 
 export default function Footer() {
   return (
-  <footer className="flex items-center bg-gray-600 dark:bg-gray-800 py-2 px-6 mt-auto">
+  <footer className="flex items-center bg-gray-100 dark:bg-gray-800 py-2 px-6 mt-auto">
     <div className="flex flex-row items-center justify-between w-full max-w-5xl mx-auto">
       <div className="flex flex-row space-x-2">
         <IconLink label="Twitterのリンク" link="https://twitter.com/object1037" >
-          <FaTwitter />
+          <FiTwitter />
         </IconLink>
         <IconLink label="GitHubのリンク" link="https://github.com/object1037" >
-          <FaGithub />
+          <FiGithub />
         </IconLink>
       </div>
-      <div className="text-gray-200 font-light text-xs">&copy; 2021 object1037</div>
+      <div className="text-gray-800 dark:text-gray-200 font-light text-xs">&copy; 2021 object1037</div>
     </div>
   </footer>
   )
