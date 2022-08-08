e93151d103f5d3d5388be7a2afb63f2ffd2b3941
object1037 <contact@object1037.dev>
2022-08-08 19:18:24 +0900

    add animation on load

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,13 @@
 import clsx from 'clsx'
 import Icon from './icon'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
   const headerStyle = [
     'px-6',
     'sm:px-12',
@@ -21,7 +27,9 @@ export default function Header() {
   return (
     <header className={clsx(headerStyle)}>
       <div className="sticky top-0 max-w-5xl mx-auto flex flex-row justify-start items-center space-x-4">
-        <Icon className="w-16 h-16" /> 
+        <div className={clsx(loaded && 'bg-[#fff5f6]', 'group w-[4.8rem] h-[4.8rem] rounded-full transition duration-1000 delay-300')}>
+          <Icon className={clsx(loaded && 'fill-[#ffbac1]', "w-16 h-16 m-[0.4rem] fill-transparent transition duration-1000 delay-300")}/>
+        </div>
         <h1 className="font-semibold text-2xl m-2">object1037</h1>
       </div>
     </header>
