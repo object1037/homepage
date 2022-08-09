b95c4f67ecf720ae9a538965e4b628d7e2fcf805
object1037 <contact@object1037.dev>
2022-08-09 15:32:06 +0900

    update icon and header

--- components/icon-link.tsx
+++ components/icon-link.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 
 export default function IconLink({
   link,
-  label,
+  title,
   children,
 }: {
   link: string,
-  label: string,
+  title: string,
   children: React.ReactNode,
 }) {
   return (
     <Link href={link}>
-      <a target="_blank" rel="noopener noreferrer" aria-label={label} className="p-4 hover:text-gray-400 dark:hover:text-gray-500 transition">
+      <a target="_blank" rel="noopener noreferrer" title={title} className="p-4 hover:text-gray-400 dark:hover:text-gray-500 transition">
         <p className="text-3xl">{children}</p>
       </a>
     </Link>
