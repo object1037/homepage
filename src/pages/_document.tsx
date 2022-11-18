515299dac4811452f57fbf3496e4107751134107
object1037 <contact@object1037.dev>
2022-11-19 01:21:09 +0900

    delete unnecessary style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 font-mono">
+      <body className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         <Main />
         <NextScript />
       </body>
