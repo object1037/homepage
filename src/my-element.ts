b4c700d2f34a6684f2abda0ddf1551b081dc72dc
object1037 <contact@object1037.dev>
2024-04-12 21:49:50 +0900

    use biome

--- src/my-element.ts
+++ src/my-element.ts
@@ -2,19 +2,19 @@ import css from './my-element.css?inline'
 import { styledHtml } from './utils/html'
 
 class MyElement extends HTMLElement {
-  constructor() {
-    super()
-  }
   connectedCallback() {
     this.attachShadow({ mode: 'open' })
     this.render()
   }
   render() {
     if (!this.shadowRoot) return
-    this.shadowRoot.innerHTML = styledHtml(css, `
+    this.shadowRoot.innerHTML = styledHtml(
+      css,
+      `
     <p>Hello,Vanilla</p>
     <slot></slot>
-    `)
+    `,
+    )
   }
 }
 
