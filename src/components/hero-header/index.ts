ae800fb26f3005a3b5bd336e787690de3b46495c
object1037 <contact@object1037.dev>
2024-04-13 18:22:34 +0900

    add hero-header component

--- src/components/hero-header/index.ts
+++ src/components/hero-header/index.ts
@@ -2,7 +2,7 @@ import { render } from '../../utils/render'
 import html from './index.html?raw'
 import css from './style.css?inline'
 
-export class MyElement extends HTMLElement {
+export class HeroHeader extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: 'open' })
     render({ shadow: this.shadowRoot, html, css })
--- src/customElements.ts
+++ src/customElements.ts
@@ -1,3 +1,5 @@
+import { HeroHeader } from './components/hero-header'
 import { MyFooter } from './components/my-footer'
 
+customElements.define('hero-header', HeroHeader)
 customElements.define('my-footer', MyFooter)
