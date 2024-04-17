152ba7759e900adbdac7f79dfbf59fcc677507b6
object1037 <contact@object1037.dev>
2024-04-17 11:00:03 +0900

    add lucky mode

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,25 @@ Promise.all([
 ]).then(() => {
   document.body.classList.add('ready')
 })
+
+const colorScheme = window.matchMedia('(prefers-color-scheme: light)')
+const setColors = () => {
+  document.documentElement.style.setProperty('--bg', '#fff5f6')
+  document.documentElement.style.setProperty('--fill', '#ffbac1')
+}
+const removeColors = () => {
+  document.documentElement.style.removeProperty('--bg')
+  document.documentElement.style.removeProperty('--fill')
+}
+
+if (colorScheme.matches && Math.random() < 0.1) {
+  setColors()
+  console.log('🍀Lucky!')
+  colorScheme.addEventListener('change', (e) => {
+    if (!e.matches) {
+      removeColors()
+    } else {
+      setColors()
+    }
+  })
+}
