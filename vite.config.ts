8eece273f7ef16157df360f09b345d727f3f3d63
object1037 <contact@object1037.dev>
2025-08-02 00:47:54 +0900

    remove devServer as it is the same as vite dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { ssgPlugin } from './lib/ssgPlugin.ts'
 
 export default defineConfig({
+  appType: 'custom',
+  plugins: [ssgPlugin(), react()],
 })
