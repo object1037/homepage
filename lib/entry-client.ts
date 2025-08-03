4d18940a4e51b33bc321ebc902f0cc4a7a4a26b9
object1037 <contact@object1037.dev>
2025-08-03 21:56:19 +0900

    impl header

--- lib/entry-client.ts
+++ lib/entry-client.ts
@@ -1,1 +1,9 @@
-console.log('client!')
+const header = document.querySelector('header')
+if (header) {
+  // header.addEventListener('click', () => {
+  //   header.classList.toggle('end')
+  // })
+  setTimeout(() => {
+    header.classList.add('end')
+  }, 1000)
+}
