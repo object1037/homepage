4f19373d6e41de98da5afd8cc1f100addf703d8b
object1037 <contact@object1037.dev>
2025-08-03 23:16:30 +0900

    add lucky color

--- lib/entry-client.ts
+++ lib/entry-client.ts
@@ -1,3 +1,24 @@
+const colorScheme = window.matchMedia('(prefers-color-scheme: light)')
+const setColors = () => {
+  document.documentElement.style.setProperty('--bg', '#fff5f6')
+  document.documentElement.style.setProperty('--hair', '#ffbac1')
+}
+const removeColors = () => {
+  document.documentElement.style.removeProperty('--bg')
+  document.documentElement.style.removeProperty('--hair')
+}
+if (colorScheme.matches && Math.random() < 0.1) {
+  setColors()
+  console.log('🍀Lucky!')
+  colorScheme.addEventListener('change', (e) => {
+    if (!e.matches) {
+      removeColors()
+    } else {
+      setColors()
+    }
+  })
+}
+
 const header = document.querySelector('header')
 if (header) {
   // header.addEventListener('click', () => {
