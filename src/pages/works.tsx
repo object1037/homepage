435fc00e13d8590da2751d30d4fbf0557e497d16
object1037 <contact@object1037.dev>
2025-08-05 19:24:43 +0900

    add works content

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,4 +1,7 @@
 import type { MetaData } from '../../lib/types'
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Work from '../components/work'
 
 export const metaData: MetaData = {
   title: 'Works | object1037.dev',
@@ -6,9 +9,76 @@ export const metaData: MetaData = {
 }
 
 export default function Works() {
+  type Links = Parameters<typeof Work>[0]['links']
+  type Publications = Parameters<typeof Work>[0]['publications']
+
+  const morphKeysPubs: Publications = [
+    [
+      <>
+        <span className="underline">Koki Yamagami</span>, Masato Goto, Junichiro
+        Kadomoto, and Hidetsugu Irie. "MorphKeys: A Reconfigurable Keyboard
+        System Using Switched NFC Tags." In The 38th Annual ACM Symposium on
+        User Interface Software and Technology (UIST ’25), September 2025.
+      </>,
+      '10.1145/3746059.3747706',
+    ],
+    [
+      <>
+        <span className="underline">Koki Yamagami</span>, Masato Goto, Junichiro
+        Kadomoto, and Hidetsugu Irie. "Demonstration of MorphKeys: A
+        Reconfigurable Keyboard System Using Switched NFC Tags." In The 38th
+        Annual ACM Symposium on User Interface Software and Technology (UIST
+        Adjunct ’25), September 2025.
+      </>,
+      '10.1145/3746058.3758992',
+    ],
+  ]
+  const cardLinks: Links = [['GitHub', 'https://github.com/object1037/Card']]
+  const L4Links: Links = [
+    ['GitHub', 'https://github.com/object1037/L4'],
+    ['Blog Post', 'https://blog.object1037.dev/posts/20240614'],
+  ]
+
   return (
-    <div>
-      <h1>Hello, Works!</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Work
+          title="MorphKeys"
+          src="morphkeys.png"
+          alt="Picture of MorphKeys, demnonstrating its reconfigurability"
+          publications={morphKeysPubs}
+        >
+          <p>
+            A reconfigurable keyboard system that lets users freely arrange
+            individual keys in three dimensions to match their ergonomic needs
+            and specific usage scenarios. Each key is an independent,
+            battery-free module powered and read via near-field communication
+            (NFC) only while being pressed, enabling wireless key input
+            detection.
+          </p>
+        </Work>
+        <Work
+          title="Card"
+          src="card.jpeg"
+          alt="Picture of Card, showing its card-like design"
+          links={cardLinks}
+        >
+          <p>A PCB business card that functions as a Bluetooth trackpad.</p>
+        </Work>
+        <Work
+          title="L4"
+          src="L4.jpeg"
+          alt="Picture of L4, showing its ortholinear layout and split design"
+          links={L4Links}
+        >
+          <p>
+            A 50% wireless split keyboard with an ortholinear layout. Power can
+            be supplied via a LiPo battery or a pair of CR2032 coin batteries.
+          </p>
+        </Work>
+      </main>
+      <Footer />
+    </>
   )
 }
