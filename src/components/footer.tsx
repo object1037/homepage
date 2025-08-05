d82c2b15fba448387059c7dee6c01d692a2bda79
object1037 <contact@object1037.dev>
2025-08-05 12:03:48 +0900

    style footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,22 @@
+import { execSync } from 'node:child_process'
+
+const getGitCommitInfo = () => {
+  const stdout = execSync('git show -s --format="%h@%ci" main')
+  const [hash, date] = stdout.toString().trim().split('@')
+  return { hash, date }
+}
+
 export default function Footer() {
+  const { hash, date } = getGitCommitInfo()
   return (
     <footer>
-      <small>© object1037</small>
+      <p>© object1037</p>
+      <p className="commit-info">
+        <a href={`https://github.com/object1037/homepage/tree/${hash}`}>
+          {hash}
+        </a>{' '}
+        @ <time dateTime={date}>{date}</time>
+      </p>
     </footer>
   )
 }
