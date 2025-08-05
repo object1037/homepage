dd3a21f8d873d4d1ea8a0ad564accc0205d71bc5
object1037 <contact@object1037.dev>
2025-08-06 08:07:24 +0900

    add link to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,9 @@ import Icon from './icon'
 export default function Header() {
   return (
     <header>
-      <Icon />
+      <a className="icon" href="/">
+        <Icon />
+      </a>
       <h1>object1037</h1>
     </header>
   )
