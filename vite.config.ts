4880da9b6503d6d1b1f2ee802cb6973e121e3976
object1037 <contact@object1037.dev>
2025-08-06 10:16:31 +0900

    use mixin and support prefers-reduced-motion

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,56 @@ import { defineConfig } from 'vite'
 import { noscriptPlugin } from './lib/noscriptPlugin.ts'
 import { ssgPlugin } from './lib/ssgPlugin.ts'
 
-export default defineConfig({
-  appType: 'custom',
-  plugins: [noscriptPlugin(), ssgPlugin(), react()],
-  environments: {
-    server: {},
-  },
-  css: {
-    transformer: 'lightningcss',
-  },
-  build: {
-    cssMinify: 'lightningcss',
-    modulePreload: {
-      polyfill: false,
+export default defineConfig(() => {
+  const mixins = new Map()
+  return {
+    appType: 'custom',
+    plugins: [noscriptPlugin(), ssgPlugin(), react()],
+    environments: {
+      server: {},
     },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('noscript.css')) {
-            return 'noscript'
-          }
-          return null
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        customAtRules: {
+          mixin: {
+            prelude: '<custom-ident>',
+            body: 'style-block',
+          },
+          apply: {
+            prelude: '<custom-ident>',
+          },
+        },
+        visitor: {
+          Rule: {
+            custom: {
+              mixin(rule) {
+                mixins.set(rule.prelude.value, rule.body.value)
+                return []
+              },
+              apply(rule) {
+                return mixins.get(rule.prelude.value)
+              },
+            },
+          },
         },
       },
     },
-  },
+    build: {
+      cssMinify: 'lightningcss',
+      modulePreload: {
+        polyfill: false,
+      },
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('noscript.css')) {
+              return 'noscript'
+            }
+            return null
+          },
+        },
+      },
+    },
+  }
 })
