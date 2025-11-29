eabf7897d63886e323de55c76dc00856784e065e
object1037 <contact@object1037.dev>
2025-11-29 15:01:07 +0900

    update packages

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@ import Icon from './icon'
 
 export default function Header({ heading }: { heading: string }) {
   return (
-    // biome-ignore lint/correctness/useUniqueElementIds: component will be rendered one time
     <header id="transition">
       <div className="container">
         <a className="icon" href="/">
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,7 +10,6 @@ export const metaData: MetaData = {
 export default function Index() {
   return (
     <>
-      {/** biome-ignore lint/correctness/useUniqueElementIds: page will be rendered one time */}
       <main id="transition">
         <Icon />
         <h1>404 NOT FOUND</h1>
